a27ccdee81f02a92906578a7ecf2fa79a4bbd3dd
Jasper Burroughs <jasperjames8@icloud.com>
2025-05-01 22:00:06 -0400

    Continued to work on software to help create dataset

--- Dataset/buildData.py
+++ Dataset/buildData.py
@@ -1,29 +1,38 @@
 import cv2 as cv
 import os
 
+def getAngles(frame):
+  return [1, 2, 3]
+
+
 # Receive the paths of each video
-path = "/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Videos"
+path = "/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Edited_Videos"
 fileNames = os.listdir(path)
 paths = []
 for filename in fileNames:
   paths.append(os.path.join(path, filename))
-
+paths.remove("/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Edited_Videos/.DS_Store")
 # Capture Each video
 for vid in paths:
   capture = cv.VideoCapture(vid)
   check = True
+  currFrame = 0
+  fps = capture.get(cv.CAP_PROP_FPS)
   while check:
     check, frame = capture.read()
-    print(check)
     if not check:
       break
-    fps = capture.get(cv.CAP_PROP_FPS)
-    print(fps)
-    #cv.imshow("frame", frame)
-    if cv.waitKey(1) == ord('q'):
+    cv.imshow("frame", frame)
+    cv.waitKey(2)
+    binary = int(input("Is this form good or bad? 1 for good, 0 for bad, 2 for do not include: "))
+    if binary == 2:
       break
+    angles = getAngles(frame)
+    with open("squatData.csv", "a") as data:
+      data.write(str(currFrame) + ", " + str(angles) + ", " + str(binary) + "\n")
+    currFrame += 1
 
-# TODO: decrease the frame rate of the videos and make them go frame by frame to allow for human input
+# TODO: Make the function to calculate the angles
 
 capture.release()
 cv.destroyAllWindows()
--- Dataset/getVideos.py
+++ Dataset/getVideos.py
@@ -1,15 +1,85 @@
 import yt_dlp as ydl
+import cv2 as cv
+import os
 
-# Open the links file
-with open("links.txt", "r") as theLinks:
-    links = theLinks.read().split("\n")
+def downloadOrig():
+    # Open the links file
+    with open("links.txt", "r") as theLinks:
+        links = theLinks.read().split("\n")
 
-# Configuring ydl options
-ydl_opts = {
-    'format': 'bestvideo[height<=720]+bestaudio/best',
-    'outtmpl': '/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Videos/%(title)s.%(ext)s'
-}
+    # Configuring ydl options
+    ydl_opts = {
+        'format': 'bestvideo[height<=720]+bestaudio/best',
+        'outtmpl': '/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Videos/%(title)s.%(ext)s'
+    }
 
-# download each video
-with ydl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(links)
+    # download each video
+    with ydl.YoutubeDL(ydl_opts) as yd:
+        yd.download(links)
+
+
+
+def alterVids():
+    # Alters the videos to decrease frame rate and cut to specified time
+    path = "/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Videos"
+    pathOut = "/Users/jasper/Desktop/ATFinal/AT-Final-Project/Dataset/Edited_Videos"
+    fileNames = os.listdir(path)
+    paths = []
+    for filename in fileNames:
+        paths.append(os.path.join(path, filename))
+
+    with open("times.txt", "r") as times:
+        timeStamps = times.readlines()
+
+    for i, path in enumerate(paths):
+        # Create capture object and split the start and end times for the current video
+        capture = cv.VideoCapture(path)
+        times = timeStamps[i].split(",")
+        fps = capture.get(cv.CAP_PROP_FPS)
+        # Calculate the start and end frames based on the times and fps
+        start = (int(times[0][0]) * 60 + int(times[0][2:])) * fps
+        end = (int(times[1][0]) * 60 + int(times[1][2:])) * fps
+
+        frame_width = int(capture.get(cv.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(capture.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+        # Define the codec and create VideoWriter object and create path for output
+        fourcc = cv.VideoWriter_fourcc(*"avc1")
+        outPath = pathOut + "/" + str(i) + ".mov"
+        out = cv.VideoWriter(outPath, fourcc, 30.0, (frame_width, frame_height))
+
+
+
+        check = True
+        # current frame number
+        currFrame = 0
+        # Whether to add the frame or skip
+        currAdd = 0
+        # Number of frames in the edited version
+        totalFrames = 0
+        while check:
+            check, frame = capture.read()
+            if not check:
+                break
+            # If the frame is between the start and end frames and is not being skipped
+            if start <= currFrame <= end and currAdd <= 0:
+                # Add the frame to the edited version and display the frame
+                totalFrames += 1
+                out.write(frame)
+                currAdd = 4
+                cv.imshow("frame", frame)
+            elif currFrame > end:
+                cv.destroyAllWindows()
+                break
+            currAdd -= 1
+            currFrame += 1
+        capture.release()
+        out.release()
+        print(totalFrames)
+
+
+if __name__ == "__main__":
+    downloadOrig()
+    alterVids()
+
+
